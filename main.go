a71e6558e65324418a490a8c4d92573f7e8fd50c
“abhi” <abhichouhan@proton.me>
2024-08-01 00:54:43 +0530

    grpc server created

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+
+	pb "split-pay/generated"
+	"split-pay/services"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &services.GreeterService{})
+
+	reflection.Register(s)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+	log.Println("gRPC server listening on port 50051")
 }
